d989e58b52c912ab7743f9c32733c463bd896264
Harsh Raj <me@harshraj.xyz>
2020-07-27 20:39:39 +0530

    Increment word with corrospondence to its length

--- file1.py
+++ file1.py
@@ -1,1 +1,11 @@
-print ("Hello World!")
+sentence=input().split()
+words=""
+for i in sentence:
+    word=i
+    size=len(word)
+    for j in range(0,size):
+        words+=chr(ord(word[j])+size)
+    words+=" "
+    
+
+print(words)
