77c8f2fbdbf14e49eed62b5a4881b8653870fa6e
Harsh Raj <me@harshraj.xyz>
2020-09-25 08:46:26 +0530

    Double Matrix

---
+++ Double_matrix.py
@@ -0,0 +1,20 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    
+    sum = []
+    h = 0
+    for i in range(4):
+        for j in range(3):
+            sum[h] = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+    arr.sort(sum)
+    print(sum[15])
